f31da424e7b085d27f70fc76626a54ff6b7e89f3
Nathan Rice <nrice778@gmail.com>
2020-12-28 16:04:10 -0600

    added comment instructions for CS1

--- ChallengeSet1.js
+++ ChallengeSet1.js
@@ -9,26 +9,32 @@ module.exports.areTwoNumbersTheSame = function(number1, number2) {
     throw new Error();
 }
 
+// Return the smallest of the two given numbers
 module.exports.getSmallestNumber = function(number1, number2) {
     throw new Error();
 }
 
+// Return the result of the minuend minus the subtrahend
 module.exports.subtract = function(minuend, subtrahend) {
     throw new Error();
 }
 
+// Return the sum of the two given numbers
 module.exports.add = function(number1, number2) { 
     throw new Error();
 }
 
+// Return the product of the two given numbers
 module.exports.multiply = function(number1, number2) {
     throw new Error();
 }
 
+// Return the greeting - "Hello!" - change to accomodate the given name, look at the tests to see examples
 module.exports.getGreeting = function(nameOfPerson) {
     throw new Error();
 }
 
+// Return the phrase according to the test
 module.exports.getHey = function() {
     throw new Error();
 }
